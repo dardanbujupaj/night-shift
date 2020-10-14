1ba228ccbd647bb40c83b3ddd7e18f319d88adec
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-10-14 20:07:15 +0200

    clean up diner scene.

--- scenes/Diner.gd
+++ scenes/Diner.gd
@@ -3,13 +3,13 @@ extends Node2D
 
 onready var character = $YSort/Character
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	randomize()
 	get_tree().set_group("enemies", "target", character)
-	pass # Replace with function body.
 
 
 func _unhandled_input(event):
@@ -18,9 +18,6 @@ func _unhandled_input(event):
 		if Input.is_key_pressed(KEY_R):
 			get_tree().reload_current_scene()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_ZombieTimer_timeout():
